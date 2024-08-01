0db47723233b0ced584f93835d609da41727e2ac
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-01 23:17:38 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,3 +21,29 @@ def filter_datum(
     """
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        parameters
+        ----------
+        record(logging.LogRecord): Log record to format
+
+        Return the formatted log record
+        """
+        original_message = super().format(record)
+        return filter_datum(
+            self.fields, self.REDACTION,
+            original_message, self.SEPARATOR
+        )
