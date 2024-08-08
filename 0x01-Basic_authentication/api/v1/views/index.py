bb238e1f9a7c5fbb2d8b1f52a3eeceefab6b0e67
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-08 06:28:15 +0300

    views

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,10 +25,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-
+"""
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
-    """
+    
     Return unathorized error
-    """
-    abort(401)
+    
+    abort(401)"""
