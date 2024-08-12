75635dbb2778d1a4406cb36f94d585ac61143b7b
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-12 08:55:38 +0300

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,9 +56,6 @@ def before_request_func():
     if auth.authorization_header(request) is None and \
             auth.session_cookie(request) is None:
         abort(401)
-    if user is None:
-        abort(403)
-    request.current_user = user
 
 
 @app.errorhandler(404)
