3adb626168db42f395d3058e2f7ad6f080883c72
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-13 11:12:57 +0300

    Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -11,11 +12,11 @@ class SessionDBAuth(SessionExpAuth):
     def create_session(self, user_id=None):
         """Create and store session in the db.
         """
-        sesssion_id = super().create_session(user_id)
+        session_id = super().create_session(user_id)
         if session_id is None:
             return None
 
-        user_session = UserSession(**kwargs)
+        user_session = UserSession(user_id=user_id, session_id=session_id)
         user_session.save()
 
         return session_id
@@ -23,13 +24,19 @@ class SessionDBAuth(SessionExpAuth):
     def user_id_for_session_id(self, session_id=None):
         """Retrieve the user_id based on the session ID stored in the db.
         """
-        if session_id is None:
+        try:
+            user_session = UserSession.search({"session_id": session_id})
+        except Exception:
+            return None
+        if len(user_session) <= 0:
             return None
 
-        user_session = UserSession.search({"session_id": session_id})
-        if not user_session:
+        current_time = datetime.now()
+        time_stamp = timedelta(seconds=self.session_duration)
+        exp_time = user_session[0].created_at + time_stamp
+        if exp_time < current_time:
             return None
-        return user_session.user_id
+        return user_session[0].user_id
 
     def destroy_session(self, request=None):
         """Destroy user_session from the db based on the session ID.
@@ -41,7 +48,7 @@ class SessionDBAuth(SessionExpAuth):
         if session_id is None:
             return False
 
-        user_session = UserSession.search("session_id": session_id)
+        user_session = UserSession.search({"session_id": session_id})
         if not user_session:
             return False
 
