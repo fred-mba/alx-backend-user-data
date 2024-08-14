bf7ffe7b0b6ca34b2516aee95daccce172effc58
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-14 19:22:36 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic flask app module
 """
 from user import User
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, abort
 from auth import Auth
 
 
@@ -28,6 +28,29 @@ def users():
             {"email": "{new_user.email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Logs in the user by validating credentials before creating a session
+       for the user, and store session ID as a cookie with key "session_id"
+       on the response and a JSON payload form.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(401)
+    if not AUTH.valid_login(email=email, password=password):
+        abort(401)
+    session_id = AUTH.create_session(email=email)
+    if not session_id:
+        abort(401)
+
+    response = make_response(
+        jsonify({"email": "{email}", "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
