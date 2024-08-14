48f2d20bb0ab040c73232a81f3d5cf9c6dcd4829
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-14 12:25:17 +0300

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -62,15 +62,11 @@ class DB:
         """Takes as argument a required user_id integer and arbitrary
            keyword arguments, and returns None
         """
-        try:
-            user = self._session.query(User).filter_by(id=user_id).one()
+        user = self._session.query(User).filter_by(id=user_id).one()
 
-            for key, value in kwargs.items():
-                if hasattr(user, key):
-                    setattr(user, key, value)
-                else:
-                    raise InvalidRequestError
-            self._session.commit()
-        except ValueError:
-            self._session.rollback()
-            raise
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError()
+        self._session.commit()
