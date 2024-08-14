16bfda00c80fc20858c7874451c47323aec1b5d4
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-14 12:26:42 +0300

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -68,5 +68,5 @@ class DB:
             if hasattr(user, key):
                 setattr(user, key, value)
             else:
-                raise ValueError()
+                raise ValueError
         self._session.commit()
