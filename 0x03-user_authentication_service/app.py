0de5acbade413f28fec9985f7a05977490a8e57a
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-15 22:58:36 +0300

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,9 +87,12 @@ def get_reset_password_token() -> str:
        respond with a 403 status code.
     """
     email = request.form.get('email')
+    reset_token = None
     try:
         reset_token = AUTH.get_reset_token(email)
     except ValueError:
+        reset_token = None
+    if reset_token is None:
         abort(403)
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
