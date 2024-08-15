972a6e597db5538b718c4b95171eb7f860d3ca56
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-16 00:19:08 +0300

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -101,15 +101,15 @@ def update_password() -> str:
     email = request.form.get('email')
     reset_token = request.form.get('reset_password')
     new_password = request.form.get('new_password')
-    changed_password = False
+    is_password_changed = False
     try:
         AUTH.update_password(reset_token, new_password)
-        changed_password = True
+        is_password_changed = True
     except ValueError:
-        changed_password = False
-    if not changed_password:
+        is_password_changed = False
+    if not is_password_changed:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"}), 200
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
