060cf9aab1f32fdaeedc9811a7931b18daa71ca1
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-16 15:11:26 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,10 +56,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
-                return True
-            else:
-                return False
+            return bcrypt.checkpw(
+                password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
 
